6cf8e59b929ca313933147725cdac2058a286008
gabestevens <120518846+gabestevens@users.noreply.github.com>
2023-03-09 18:50:56 -0500

    changes to js

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let object = document.createElement('img')
     object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
     document.body.append(object)
     return object
 }
 
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+function newItem(url){
+    let item = newImage(url)
+    item.addEventListener('click', function(){
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
+    return item
 }
+
+function move(element){
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left,bottom){
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+    let result= {
+        to: moveToCoordinates
+    }
+    return result
+   
+}
+
+/*let greenCharacter = newImage('assets/green-character.gif')
+let moveobject= move(greenCharacter)
+moveobject.to(100, 250)*/
 
 function newInventory(){
     let inventory = document.createElement('div')
@@ -26,16 +48,18 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
 
-newInventory()
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+const inventory = newInventory()
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+move(inventory).to(0,0)
+move(newImage('assets/green-character.gif')).to(300,300)
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
